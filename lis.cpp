0f74043d486274c37dac4f6f4586a9d2c3298575
Philip Thomas <thomasphil6889@gmail.com>
2025-05-13 09:40:10 +0530

    created a file

---
+++ lis.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Filename: lis.cpp
+// Description: Longest Increasing Subsequence (LIS) using dynamic programming (n^2 version).
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    vector<int> dp(n, 1);
+    int lis = 1;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(arr[j] < arr[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        lis = max(lis, dp[i]);
+    }
+    cout << lis << endl;
+    return 0;
+}
